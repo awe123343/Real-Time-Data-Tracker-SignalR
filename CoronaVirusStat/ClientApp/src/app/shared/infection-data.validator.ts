9ab7669b9d09682933ff02fd7444f1d044533ada
Sylvester Wang <awe1233430@hotmail.com>
2020-07-07 23:25:24 -0400

    Improve validator

--- CoronaVirusStat/ClientApp/src/app/shared/infection-data.validator.ts
+++ CoronaVirusStat/ClientApp/src/app/shared/infection-data.validator.ts
@@ -1,9 +1,13 @@
 import { AbstractControl, ValidatorFn, FormGroup, ValidationErrors } from '@angular/forms'
 
 export const infectionDataValidator: ValidatorFn = (control: FormGroup): ValidationErrors | null => {
+  if (!control.get('infectedNo').value || !control.get('recoveredNo').value || !control.get('deathNo').value) {
+    return null;
+  }
+
   const infectedNo = Number(control.get('infectedNo').value); 
   const recoveredNo = Number(control.get('recoveredNo').value);
   const deathNo = Number(control.get('deathNo').value);
 
-  return infectedNo && recoveredNo && deathNo && infectedNo >= (recoveredNo + deathNo) ? null : { 'invalidInput': true };
+  return infectedNo >= (recoveredNo + deathNo) ? null : { 'invalidInput': true };
 };
